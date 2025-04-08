0cfd6d89167f9f702a148f84c939bf4c27024fab
LuisCarretero <luis.carretero@gmx.de>
2025-04-08 12:00:33 +0200

    Renamed workdir

---
+++ GenCFD-prep/preprocess.py
@@ -0,0 +1,13 @@
+from preprocessing_utils import process_data
+
+if __name__ == '__main__':
+
+    process_data(
+        raw_data_path=r'/cluster/work/math/camlab-data/Wave_HemewS-3D/version1',
+        processed_data_path=r'/cluster/work/math/camlab-data/Wave_HemewS-3D/processed/version1',
+        S_out=32,
+        Nt=64,
+        f=10,
+        fmax=5,
+        max_files=9999
+    )
