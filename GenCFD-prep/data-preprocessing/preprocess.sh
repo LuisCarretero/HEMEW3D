a1974da18c9bfae40949065f785e03af90199003
LuisCarretero <luis.carretero@gmx.de>
2025-04-10 13:47:00 +0200

    Restructured preprocessing files

--- GenCFD-prep/data-preprocessing/preprocess.sh
+++ GenCFD-prep/data-preprocessing/preprocess.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 #SBATCH --job-name=preprocess_HEMEW3D
-#SBATCH --output=/cluster/home/lcarretero/workspace/rds/HEMEW3D/genfcd_prep/preprocess.out
+#SBATCH --output=/cluster/home/lcarretero/workspace/rds/HEMEW3D/GenCFD-prep/preprocess.out
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem-per-cpu=4G
+#SBATCH --time=4:00:00
 
 # NB: preprocess.py bottlenecks are currently single-threaded anyways so no need to increase cpu count
 
 source $HOME/python_envs/rds-misc/bin/activate
 
-cd $HOME/workspace/rds/HEMEW3D/genfcd_prep
+cd $HOME/workspace/rds/HEMEW3D/GenCFD-prep
 
 echo ":- Starting preprocessing job..."
 echo ":- Python environment: $VIRTUAL_ENV"
